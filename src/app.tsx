367bfc5e7546037a547d02e27cead4f278682b21
deusv0lt <sohan@sohanmanju.com>
2023-04-10 10:27:39 -0700

    removed unecessary imports

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import styled from 'styled-components'
 import Sidebar from './components/Sidebar'
 import WorldMap from './components/WorldMap'
